34870bcd528ce6c9b080b17195fb2d6351e87155
rmesh123 <rdanuwar1994@gmail.com>
2022-02-12 21:43:33 -0600

    Update settings.py

--- efsd2/efs/efs/settings.py
+++ efsd2/efs/efs/settings.py
@@ -122,6 +122,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
 
